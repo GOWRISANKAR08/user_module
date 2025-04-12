eb1d73b2997a07a92ad053f7d0b0b5d42e2762f9
GOWRISANKAR08 <sankar08garu@gmail.com>
2025-04-12 19:01:52 +0530

    user module updates

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="user_module",
-    version="1.0.0",
-    description="Reusable Django app for user management.",
-    author="Your Name",
-    author_email="your@email.com",
-    url="https://github.com/your-org/user_module",
-    packages=find_packages(),  # auto detects packages
+    name='user_module',
+    version='1.0.0',
+    packages=find_packages(),  # This finds user_module/
     include_package_data=True,
-    install_requires=[
-        "Django>=3.2"
-    ],
-    classifiers=[
-        "Framework :: Django",
-        "Programming Language :: Python :: 3",
-    ],
+    install_requires=[],
+    zip_safe=False,
 )
