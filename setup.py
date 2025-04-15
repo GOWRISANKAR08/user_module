a657d964e52605bc3e2a02dc2ed55528b1942e2b
GOWRISANKAR08 <sankar08garu@gmail.com>
2025-04-15 11:44:44 +0530

    user Initial

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='user_module',
-    version='1.0.0',
-    packages=find_packages(),  # This finds user_module/
-    include_package_data=True,
-    install_requires=[],
-    zip_safe=False,
+    name="django-user-module",  # PyPI compatible name
+    version="0.1.0",  # Initial version (semantic versioning)
+    packages=find_packages(),
+    include_package_data=True,  # Required for Django templates/static files
+    install_requires=[
+        "Django>=5.0",  # Django 5.0+ has official Python 3.13 support
+        # Add other dependencies if needed
+    ],
+    python_requires=">=3.13",  # Explicit Python version lock
+    classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "Framework :: Django :: 5.0",
+        "Operating System :: OS Independent",
+    ],
 )
