21470b4d94e705a15c8697d19dc05d72deefa363
GOWRISANKAR08 <sankar08garu@gmail.com>
2025-04-15 23:19:11 +0530

    user Initial

--- user_module/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet
-
-urlpatterns = [
-    path('', views.UserProfileViewSet, name='user-home'),
-]
